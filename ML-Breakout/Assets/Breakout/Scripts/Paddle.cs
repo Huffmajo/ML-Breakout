1ed1a31806fc20883b086d980f9244b0a9bf085c
Jeff <dickinsj@oregonstate.edu>
2020-01-27 19:37:39 -0600

    added bounds to paddle movement
    see summary

--- ML-Breakout/Assets/Breakout/Scripts/Paddle.cs
+++ ML-Breakout/Assets/Breakout/Scripts/Paddle.cs
@@ -6,6 +6,8 @@ public class Paddle : MonoBehaviour
 {
 
     public float paddleSpeed = 20f;
+    public float xPosLimit = 5f;
+    public float xNegLimit = -5f;
 
 
     // Start is called before the first frame update
@@ -17,6 +19,14 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if outside higher x axis range, reset back to range limit
+        if (transform.position.x > xPosLimit)
+            transform.position = new Vector3(xPosLimit, transform.position.y, transform.position.z);
+
+        //if outside lower x axis range, reset back to range limit
+        if (transform.position.x < xNegLimit)
+            transform.position = new Vector3(xNegLimit, transform.position.y, transform.position.z);
+
         transform.Translate( 0f, Input.GetAxis("Horizontal") * Time.deltaTime * paddleSpeed, 0f);
     }
 }
