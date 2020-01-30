dd8542a29eb9fd904a8f451caeb51ae28f0e61fd
Huffmajo <huffmajo@oregonstate.edu>
2020-01-29 20:45:37 -0800

    At start of game, ball hovers above paddle until released by user. Added small security measures to prevent ball from getting stuck.

--- ML-Breakout/Assets/Breakout/Scripts/Ball.cs
+++ ML-Breakout/Assets/Breakout/Scripts/Ball.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-	public float ballSpeed = 2f;
+	public float ballSpeed = 20f;
+	public float startingSpeed = 20f;
 	public Vector3 ballDirection;
 	public Vector3 ballVelocity;
+	public GameObject paddle;
 
 	private float horiDirection = 1f;
 	private float vertDirection = 1f;
 	private Rigidbody rb;
+	private bool heldByPaddle;
+	private Vector3 heldBallPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -19,39 +23,85 @@ public class Ball : MonoBehaviour
     	// get ball rigidbody
     	rb = GetComponent<Rigidbody>();
 
+    	// ball speed is starting speed
+    	ballSpeed = startingSpeed;
+
     	// set starting velocity directions
         horiDirection = 1;
         vertDirection = 1;
         ballDirection = new Vector3(horiDirection, vertDirection, 0f);
 
-        // start ball moving, bouncy physics material will keep it moving
-        rb.velocity = ballDirection * ballSpeed;
+        // ball starts docked to paddle
+        heldByPaddle = true;
+        heldBallPosition = new Vector3(paddle.transform.position.x, paddle.transform.position.y + 2, paddle.transform.position.z);
+        transform.position = heldBallPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-    	//ballMovement = new Vector3(hori, vert, 0f);
-    	//transform.position += ballMovement;
-        //rb.AddForce(new Vector3(ballSpeed, ballSpeed, 0f));
+    	// get input for ball release
+    	bool releaseButton = Input.GetButtonDown("Jump");
+
+    	// ball is held in front of paddle until released
+    	if (heldByPaddle)
+    	{
+    		heldBallPosition = new Vector3(paddle.transform.position.x, paddle.transform.position.y + 2, paddle.transform.position.z);
+    		transform.position = heldBallPosition;
+
+    		// release ball from user input
+    		if (releaseButton)
+    		{
+    			heldByPaddle = false;
+
+	    		// start ball moving
+    	    	rb.velocity = ballDirection * ballSpeed;
+    		}
+    	}
+    	// otherwise ball is released and play is active
+    	else
+    	{
+    		// if ball has little to no y velocity, give it some
+    		float absVelocityY = Mathf.Abs(rb.velocity.y);
+
+    		if (absVelocityY < 1f)
+    		{
+    			ballDirection = new Vector3(rb.velocity.x, 3f, rb.velocity.z);
+    			rb.velocity = ballDirection;
+    			Debug.Log("Y velocity too low, added some vertical oof!");
+    		}
+    	}
 
         // track velocity for interactions with bricks and paddle
         ballVelocity = rb.velocity;
     }
 
+    // ball bounces toward side of paddle hit, bounces as usual in center of paddle
     void OnCollisionEnter(Collision col)
     {
-    	// bounce off paddle is dependent on proximity to paddle center
-    	if (col.gameObject.tag == "Paddle")
+    	// leftside hit makes ball bounce left
+    	if (col.gameObject.tag == "paddle_left")
     	{
-    		Debug.Log("Ball hit Paddle");
-    		// insert bounce equations here once paddle prefab is available
+    		Debug.Log("Leftside paddle hit");
+    		
+    		// if ball is moving right, reverse x velocity
+    		if (rb.velocity.x > 0)
+    		{
+				ballDirection = new Vector3((rb.velocity.x * -1), rb.velocity.y, rb.velocity.z);
+				rb.velocity = ballDirection;
+    		}
     	}
-    	// bounce off any other surface as usual
-    	else
+    	// rightside hit makes ball bounce right
+    	else if (col.gameObject.tag == "paddle_right")
     	{
-	    	horiDirection *= -1;
-    		vertDirection *= -1;
+    		Debug.Log("Rightside paddle hit");
+    		
+    		// if ball is moving right, reverse x velocity
+    		if (rb.velocity.x < 0)
+    		{
+				ballDirection = new Vector3((rb.velocity.x * -1), rb.velocity.y, rb.velocity.z);
+				rb.velocity = ballDirection;
+    		}	
     	}
     }
 }
