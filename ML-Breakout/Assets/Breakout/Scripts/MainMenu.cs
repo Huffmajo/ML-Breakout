7d39fd600ee7e895aa279d05ef6466760d437d93
Huffmajo <huffmajo@oregonstate.edu>
2020-02-02 15:05:28 -0800

    Main menu buttons working. Options buttons are next.

--- ML-Breakout/Assets/Breakout/Scripts/MainMenu.cs
+++ ML-Breakout/Assets/Breakout/Scripts/MainMenu.cs
@@ -10,12 +10,18 @@ public class MainMenu : MonoBehaviour
 	// loads level select scene
 	public void LevelSelect()
 	{
-		//SceneManager.LoadScene(<level select scene name>);
+		// loads first level
+		// TODO: change this to load the level select screen once implemented
+		SceneManager.LoadScene("Breakout Level 01");
 	}
 
 	// exits game
 	public void Quit()
 	{
+		// close application when run from executable
 		Application.Quit();
+
+		// close down editor when running from editor
+		UnityEditor.EditorApplication.isPlaying = false;
 	}
 }
