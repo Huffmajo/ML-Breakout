a24fde08387ae90de5d34a97cb1654600ec266df
Huffmajo <huffmajo@oregonstate.edu>
2020-02-01 21:09:49 -0800

    Started main menu

---
+++ ML-Breakout/Assets/Breakout/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	// loads level select scene
+	public void LevelSelect()
+	{
+		//SceneManager.LoadScene(<level select scene name>);
+	}
+
+	// exits game
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
