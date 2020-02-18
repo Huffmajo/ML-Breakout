eeb35f48bf4c70d34c1005aac1605cd7544dd468
Jeff <dickinsj@oregonstate.edu>
2020-02-17 20:01:37 -0600

    tough bricks change color now after each hit

--- ML-Breakout/Assets/Breakout/Scripts/Brick.cs
+++ ML-Breakout/Assets/Breakout/Scripts/Brick.cs
@@ -9,6 +9,11 @@ public class Brick : MonoBehaviour
 	public PaddleAgent paddleAgent;
 	private int collisionCount = 0;
 
+	private int counter = 0;
+	public List<Color> colors;	//colors defined in inspector window for prefabs
+
+
+
 	//check for collisions and destroy brick
 	void OnCollisionEnter(Collision other)
 	{
@@ -18,6 +23,17 @@ public class Brick : MonoBehaviour
 		{
 			Destroy(gameObject);
 		}
+		else if (maxCollisions > 0 && counter < colors.Count-1)
+		{
+			counter++;
+			
+			var brickRenderer = gameObject.GetComponent<Renderer>();
+			brickRenderer.material.SetColor("_Color", colors[counter]);
+
+		}
+
+
+
 
 		if (training)
 		{
