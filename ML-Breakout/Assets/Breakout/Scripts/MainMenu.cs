0581cc02f895b1ae15424088e019487d680390dd
Tera Schaller <tschaller722@gmail.com>
2020-03-10 12:55:49 -0500

    attempted to fix main menu demo bug - unsuccessful

--- ML-Breakout/Assets/Breakout/Scripts/MainMenu.cs
+++ ML-Breakout/Assets/Breakout/Scripts/MainMenu.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	private GameObject aiBall;
+	private Ball aiBallScript;
 
 	// loads level select scene
 	public void level01()
@@ -21,6 +23,38 @@ public class MainMenu : MonoBehaviour
 	public void level03()
 	{
 		SceneManager.LoadScene("Versus03");
+	}
+
+	//function called when main menu reloads 
+	public void OnEnable()
+	{
+		Debug.Log("enable main menu script");
+		//what happens if I reload entire scene??
+		//NO!!! don't do that --> infinite loop
+		//BAD IDEA
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+		//find the current AI ball in the scene
+		aiBall = GameObject.FindWithTag("AIBall");
+		//aiBall = GameObject.Find("/AIArea/AIBall");
+		if (aiBall == null)
+		{
+			Debug.Log("AIBall not found");
+		}
+		else
+		{
+			//use ball functions to launch or reset the ball
+			aiBallScript = aiBall.GetComponent<Ball>();
+			//aiBallScript.ResetBall();
+			aiBallScript.LaunchBall(20f, 45f);
+		}
+		
+		
+	}
+
+	public void OnDisable()
+	{
+		Debug.Log("script disabled");
 	}
 
 
